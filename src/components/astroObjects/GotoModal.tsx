05dc176e75b1d196a5f7a59d3d21a603569cd24d
stevejcl <stevejcl@users.noreply.github.com>
2024-04-03 18:34:48 +0200

    add sliding pane for Log view
    Add sliding pane : need a button to open it instead of Goto

--- src/components/astroObjects/GotoModal.tsx
+++ src/components/astroObjects/GotoModal.tsx
@@ -1,6 +1,5 @@
 import type { Dispatch, SetStateAction } from "react";
-import Modal from "react-bootstrap/Modal";
-
+import SlidingPane from "react-sliding-pane";
 import { AstroObject } from "@/types";
 
 type PropTypes = {
@@ -16,19 +15,22 @@ type Message = {
 export default function GotoModal(props: PropTypes) {
   const { showModal, setShowModal, object, messages, setMessages } = props;
 
-  function handleCloseModal() {
-    setShowModal(false);
-    setMessages([] as Message[]);
-  }
-
   return (
-    <Modal show={showModal} onHide={handleCloseModal}>
-      <Modal.Header closeButton>
-        <Modal.Title>{object.displayName}</Modal.Title>
-      </Modal.Header>
-
-      <Modal.Body>
-        <div className="mb-3-modal">
+    <div>
+      <SlidingPane
+        className="some-custom-class"
+        overlayClassName="some-custom-overlay-class"
+        isOpen={showModal}
+        title={object.displayName}
+        from="right"
+        width="26%"
+        onRequestClose={() => {
+          // triggered on "<" on left top click or on outside click
+          setShowModal(false);
+          setMessages([] as Message[]);
+        }}
+      >
+        <div className="mb-3">
           RA: {object.ra}, Dec: {object.dec}
         </div>
 
@@ -36,7 +38,7 @@ export default function GotoModal(props: PropTypes) {
         <div>
           <pre>{JSON.stringify(messages, null, 2)}</pre>
         </div>
-      </Modal.Body>
-    </Modal>
+      </SlidingPane>
+    </div>
   );
 }

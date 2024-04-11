ee740d61a023edb87fc024d65ad3e2b1adc68870
acocalypso <st.luzifer6663@hotmail.com>
2024-04-11 12:01:13 +0200

    change forecast layout to div

--- src/pages/weather.tsx
+++ src/pages/weather.tsx
@@ -4,7 +4,7 @@ import Weather from "@/components/Weather";
 export default function WeatherForeCast() {
   return (
     <div className="daily-horp d-inline-block w-100">
-      <div className="container">
+      <div className="forecast-container-main">
         <h1>Weather Forecast</h1>
         <Weather />
       </div>

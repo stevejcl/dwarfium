042d3bbaa315433fc5be5b95b4ae001e39ae1651
acocalypso <st.luzifer6663@hotmail.com>
2024-04-12 00:15:28 +0200

    add placeholder image of no stacked_thumbnail is available

--- src/pages/weather.tsx
+++ src/pages/weather.tsx
@@ -3,13 +3,13 @@ import Weather from "@/components/Weather";
 
 export default function WeatherForeCast() {
   return (
-      <section className="daily-horp d-inline-block w-100">
-          <div className="container">
-      <div className="forecast-container-main">
-        <h1>Weather Forecast</h1>
-        <Weather />
-              </div>
+    <section className="daily-horp d-inline-block w-100">
+      <div className="container">
+        <div className="forecast-container-main">
+          <h1>Weather Forecast</h1>
+          <Weather />
         </div>
+      </div>
     </section>
   );
 }

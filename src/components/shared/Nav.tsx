91292deb6c88715b76a4314db2cd0e8b1ba7969e
acocalypso <st.luzifer6663@hotmail.com>
2024-04-24 15:35:11 +0200

    reflect version number in version modal

--- src/components/shared/Nav.tsx
+++ src/components/shared/Nav.tsx
@@ -6,7 +6,7 @@ export default function Nav() {
   const [modalOpen, setModalOpen] = useState(false);
   const [devEnabled, setDevEnabled] = useState(false);
   const [navbarOpen, setNavbarOpen] = useState(false);
-  const versionNumber = "2.2";
+  const versionNumber = "2.2.0";
 
   useEffect(() => {
     const devState = localStorage.getItem("devState");
